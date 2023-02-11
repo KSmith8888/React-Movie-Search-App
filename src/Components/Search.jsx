43d71341763e9a0b55e0b073f08c9e802e6fb85d
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-10 17:55:04 -0800

    Completed search functionality
    Added prod URL

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,36 +1,60 @@
 import { useState } from "react";
+import { Movie } from "./Movie";
 
 function Search(props) {
-    const [searchText, setSearchText] = useState("");
+    const [search, setSearch] = useState("");
 
     function handleSubmit(event) {
         event.preventDefault();
-        fetch(`http://127.0.0.1:3000/movies/${searchText}`)
+        fetch(
+            `https://api-proxy-server-production-dfe7.up.railway.app/movies/${search}`
+        )
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                console.log(data);
+                props.setMovieData(
+                    data.Search.map((film) => {
+                        return (
+                            <Movie
+                                key={film.imdbID}
+                                title={film.Title}
+                                year={film.Year}
+                                image={film.Poster}
+                                id={film.imdbID}
+                            />
+                        );
+                    })
+                );
             })
             .catch((err) => {
                 console.error(err);
+                props.setMovieData(
+                    <h2 className="error-text">
+                        Sorry, there has been an error getting the requested
+                        data. Please try again later.
+                    </h2>
+                );
             });
-        setSearchText("");
+        setSearch("");
     }
 
     return (
         <form
+            className="search-form"
             onSubmit={(e) => {
                 handleSubmit(e);
             }}
         >
-            <label labelFor="search-input">Search Movies:</label>
+            <label htmlFor="search-input" className="search-label">
+                Search Movies:
+            </label>
             <input
                 id="search-input"
                 type="search"
-                value={searchText}
+                value={search}
                 onChange={(e) => {
-                    setSearchText(e.target.value);
+                    setSearch(e.target.value);
                 }}
             />
             <button type="submit" className="search-button">
