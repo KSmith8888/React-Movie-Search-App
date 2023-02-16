677360a842ddba10b5aa518c309baf8175079a6e
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-16 15:42:06 -0800

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { Search } from "./Search";
 function Header(props) {
     return (
         <header className="header-section">
-            <h1 className="main-heading">React Movie App</h1>
+            <h1 className="main-heading">React Movie Search App</h1>
             <Search setMovieData={props.setMovieData} />
         </header>
     );
